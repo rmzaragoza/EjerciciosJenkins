e91edd50c7aa9f83ec14003dc0163fad1eca6e82
rmzaragoza <167966436+rmzaragoza@users.noreply.github.com>
2024-04-24 14:03:28 +0200

    Add files via upload

---
+++ miproyectoMaven1/src/main/java/rosa/miproyecto/MiproyectoApplication.java
@@ -0,0 +1,13 @@
+package rosa.miproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiproyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiproyectoApplication.class, args);
+	}
+
+}
---
+++ miproyectoMaven1/src/test/java/rosa/miproyecto/MiproyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package rosa.miproyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiproyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
