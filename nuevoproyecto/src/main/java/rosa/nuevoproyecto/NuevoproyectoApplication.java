9ce6e4aa8c32237750e088f30f27509be609f6e5
rmzaragoza <167966436+rmzaragoza@users.noreply.github.com>
2024-04-25 12:50:47 +0200

    Add files via upload

---
+++ nuevoproyecto/src/main/java/rosa/nuevoproyecto/NuevoproyectoApplication.java
@@ -0,0 +1,13 @@
+package rosa.nuevoproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuevoproyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuevoproyectoApplication.class, args);
+	}
+
+}
---
+++ nuevoproyecto/src/test/java/rosa/nuevoproyecto/NuevoproyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package rosa.nuevoproyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuevoproyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
